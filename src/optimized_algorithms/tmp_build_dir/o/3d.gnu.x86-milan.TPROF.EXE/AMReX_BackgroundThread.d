3827ad5710bf235c844803799895483b6ea2f888
Sowmya Yellapragada <sowmyay2@login23.chn.perlmutter.nersc.gov>
2025-06-25 20:26:40 -0700

    cleanup

--- src/optimized_algorithms/tmp_build_dir/o/3d.gnu.x86-milan.TPROF.EXE/AMReX_BackgroundThread.d
+++
@@ -1,6 +0,0 @@
-tmp_build_dir/o/3d.gnu.x86-milan.TPROF.EXE/AMReX_BackgroundThread.o: \
- ../../../amrex/Src/Base/AMReX_BackgroundThread.cpp \
- ../../../amrex-install/include/AMReX_BackgroundThread.H \
- tmp_build_dir/s/3d.gnu.x86-milan.TPROF.EXE/AMReX_Config.H
-../../../amrex-install/include/AMReX_BackgroundThread.H:
-tmp_build_dir/s/3d.gnu.x86-milan.TPROF.EXE/AMReX_Config.H:
